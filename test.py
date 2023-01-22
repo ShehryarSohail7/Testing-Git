b38d3c1728c9fbaadba04251f3ac7bcacf716e3b
Shehryar Sohail <sheryboyrkoo@gmail.com>
2023-01-22 22:00:25 +0500

    branching

--- test.py
+++ test.py
@@ -9,3 +9,11 @@ print("new")
 # git reset --hard <hash>
 
 # git push (for remote bcz my github is connected)
+
+# git branch
+# git checkout -b "label branch" (creates)(this also takes you to that branch)
+# git merge <branch label>
+#                   e.g: git checkout main (moving between branches)
+
+# merge (the branch that you want to merge into; first switch to that branch and then perform merge)
+# git merge <branch label>
