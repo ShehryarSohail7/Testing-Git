b6d803a0b80e6f39915a8bc76b58488a2c9e72ee
Shehryar Sohail <sheryboyrkoo@gmail.com>
2023-01-22 20:53:15 +0500

    the end

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import pandas as pd
-print("fff")
+print("completed")
 print("new")
 # git init
 # git add .
-# git commit -m "label name"
+# git commit -m "label name" (for local)
 # git log
+# git status
 # git reset --hard <hash>
+
+# git push (for remote bcz my github is connected)
